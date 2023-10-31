e10dfc6308e9e17c3af313e2904f33bc59a27b11
shanedbutler <shanedbutler@gmail.com>
2023-10-31 16:44:37 -0400

    Add result models

---
+++ Models/CsvParsingResults.cs
@@ -0,0 +1,13 @@
+﻿namespace CsvValidationTool.Models
+{
+    public class CsvParsingResults
+    {
+        public CsvParsingResults()
+        {
+            Emails = new List<string>();
+            Errors = new List<string>();
+        }
+        public List<string> Emails { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ Models/EmailValidationResults.cs
@@ -0,0 +1,13 @@
+﻿namespace CsvValidationTool.Models
+{
+    public class EmailValidationResults
+    {
+        public EmailValidationResults()
+        {
+            ValidEmails = new List<string>();
+            InvalidEmails = new List<string>();
+        }
+        public List<string> ValidEmails { get; set; }
+        public List<string> InvalidEmails { get; set; }
+    }
+}
